e7acaae0c22f861528f87766d60896664bb2dc01
felipefm12 <jorge.flores.m@tecsup.edu.pe>
2024-09-23 20:19:11 -0500

    Initial Commit

---
+++ HuillcaSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HuillcaSnapchat
+//
+//  Created by Mac 06 on 23/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
